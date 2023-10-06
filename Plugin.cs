02a46b8d151749b83e7f6d1941d8d9a6395e5af7
Romsstar <romsstar@gmail.com>
2023-10-07 00:15:14 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnlockEggs;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "Romsstar.DWNO.UnlockEggs";
+    internal const string PluginName = "UnlockEggs";
+    internal const string PluginVersion = "1.0.0";
+
+    public override void Load()
+    {
+        Awake();
+    }
+
+    public void Awake()
+    {
+        Harmony harmony = new Harmony("Patches");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(uRebirthPanel), "enablePanel")]
+    [HarmonyPostfix]
+    public static void Postfix(uRebirthPanel __instance, bool enable)
+    {
+        uint flagSetId = Language.makeHash("flagset_166");
+        StorageData.m_ScenarioProgressData.SetScenarioFlagByFlagSet(flagSetId, true);
+
+        __instance.eggMax = 13;
+        __instance.m_eggTbl = new int[__instance.eggMax];
+
+        int i;
+        for (i = 0; i < 8; i++)
+        {
+            __instance.m_eggTbl[i] = i + 1;
+        }
+        __instance.m_eggTbl[i++] = 10;
+        __instance.m_eggTbl[i++] = 12;
+        __instance.m_eggTbl[i++] = 9;
+        __instance.m_eggTbl[i++] = 13;
+        __instance.m_eggTbl[i++] = 14;
+        __instance.InitializeDiagram(0, true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# DWNO UnlockEggs
+This mod unlocks the Kuramon and DLC Eggs of the game right at the start of the game, without having to fullfill the requirements.
+The code was not thoroughly tested and might cause issues. A backup of your savegame prior to attempting this mod is recommended.
+
+# Installation
+1. Install BepInEx (Tutorial by Yasha-jin)
+2. Download the latest release from the Releases Tab
+3. Copy DWNO_UnlockEggs into the plugins folder of BepinEx
+4. Start the game and enjoy :)
+
+# Contact
+Discord: Romsstar#8098, or in either the Digimon Modding Community or Digimon Discord Community
